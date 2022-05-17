83172787313879b10f9ae1c100d094d685947918
kh-Golden-Retriever <horicami.ken2@gmail.com>
2022-05-17 23:01:41 +0900

    add crud to gift

--- app/models/gift.rb
+++ app/models/gift.rb
@@ -1,3 +1,7 @@
 class Gift < ApplicationRecord
+  belongs_to :user, dependent: :destroy
+
+  validates :title, presence: true
+
   enum status: {draft:0, display:1, undisplay:2, done_deal:3}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
 
+  has_many :gifts
+
   validates :name, presence: true
   validates :email, uniqueness: true, presence: true
 
